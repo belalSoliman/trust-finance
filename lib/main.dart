72f7d1fbed51da75b52f62af142ffa4d22babfcd
belal_soliman <belalsoliman8@gmail.com>
2025-02-21 16:10:31 +0200

    add Firebase integration with Firestore support and configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:trust_finiance/firebase_options.dart';
 import 'package:trust_finiance/trust_app.dart';
 
 void main() async {
@@ -7,6 +9,9 @@ void main() async {
   FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
 
   // Your initialization code here
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   // Remove splash screen when initialization is complete
   FlutterNativeSplash.remove();
